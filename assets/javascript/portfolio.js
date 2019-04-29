3dc8fc1d2e4bb9e405f3321bcf35b625fbd5476d
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-04-28 23:49:12 -0500

    redesigned portfolio page.  Added cards for each project and embedded video.  Included modal with details..updated links on contact page to linkedin and ig page.  Also switched resume to an updated version.

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,13 @@
+
+// $(document).ready(function() {
+//     console.log( "ready!" );
+//     $(".card").flip({
+//         speed: 900,
+//         axis: "x"
+//     });
+
+    // $(".card").on('click',function(){
+    //     $(".video").toggle();
+    //   });
+
+// });
